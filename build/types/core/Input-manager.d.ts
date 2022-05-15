0aca2741bc6ecc870cde0c46f00eb1b5db94057d
Gui Seek <guiseek@gmail.com>
2022-05-15 00:16:31 -0300

    fix: build

--- build/types/core/Input-manager.d.ts
+++ build/types/core/Input-manager.d.ts
@@ -1,31 +1,35 @@
-import { World } from '../world/World';
 import { IInputReceiver } from '../interfaces/iinput-receiver';
 import { IUpdatable } from '../interfaces/iupdatable';
+import { World } from '../world/World';
 export declare class InputManager implements IUpdatable {
     updateOrder: number;
     world: World;
-    domElement: any;
+    domElement: HTMLElement;
     pointerLock: any;
     isLocked: boolean;
     inputReceiver: IInputReceiver;
-    boundOnMouseDown: (evt: any) => void;
-    boundOnMouseMove: (evt: any) => void;
-    boundOnMouseUp: (evt: any) => void;
-    boundOnMouseWheelMove: (evt: any) => void;
-    boundOnPointerlockChange: (evt: any) => void;
+    boundOnMouseDown: (evt: MouseEvent) => void;
+    boundOnMouseMove: (evt: MouseEvent) => void;
+    boundOnMouseUp: (evt: MouseEvent) => void;
+    boundOnMouseWheelMove: (evt: WheelEvent) => void;
+    boundOnPointerlockChange: (evt: Event) => void;
     boundOnPointerlockError: (evt: any) => void;
-    boundOnKeyDown: (evt: any) => void;
-    boundOnKeyUp: (evt: any) => void;
+    boundOnKeyDown: (evt: KeyboardEvent) => void;
+    boundOnKeyUp: (evt: KeyboardEvent) => void;
     constructor(world: World, domElement: HTMLElement);
     update(timestep: number, unscaledTimeStep: number): void;
     setInputReceiver(receiver: IInputReceiver): void;
     setPointerLock(enabled: boolean): void;
-    onPointerlockChange(event: MouseEvent): void;
+    onPointerlockChange(event: Event): void;
     onPointerlockError(event: MouseEvent): void;
     onMouseDown(event: MouseEvent): void;
     onMouseMove(event: MouseEvent): void;
     onMouseUp(event: MouseEvent): void;
     onKeyDown(event: KeyboardEvent): void;
     onKeyUp(event: KeyboardEvent): void;
+    /**
+     * @Todo use or remove
+     */
+    handleEvent<K extends keyof GlobalEventHandlersEventMap>(): (event: GlobalEventHandlersEventMap[K]) => void;
     onMouseWheelMove(event: WheelEvent): void;
 }
